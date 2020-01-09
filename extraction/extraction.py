e032ff787860d8ae8292fa1c701dedf20f3d7311
Rohit Rajat Chattopadhyay <rohit.chattopadhyay1@gmail.com>
2020-01-09 16:37:32 +0530

    test upload

--- extraction/extraction.py
+++ extraction/extraction.py
@@ -62,21 +62,21 @@ if args.path:
         link = "ftp://ftp.ncbi.nlm.nih.gov/pub/pmc/oa_bulk/" + file
 
         print("Downloading\t\t:\t",file)
-        # cmd = "curl -o ./source.tar.gz \"%s\"" % link
-        # os.system(cmd)
+        cmd = "curl -o ./source.tar.gz \"%s\"" % link
+        os.system(cmd)
 
-        # cmd = "mkdir papers"
-        # os.system(cmd)
-        # print("Extracting",file)
-        # cmd = "tar -xzf ./source.tar.gz -C ./papers/ -T ./temp_journals_list"
+        cmd = "mkdir papers"
+        os.system(cmd)
+        print("Extracting",file)
+        cmd = "tar -xzf ./source.tar.gz -C ./papers/ -T ./temp_journals_list"
         print("Extracting\t\t:\t",file)
-        # os.system(cmd)
-        # with open("extraction_restart.log", "a") as log:
-        #     log.write(file+"\n")        
+        os.system(cmd)
+        with open("extraction_restart.log", "a") as log:
+            log.write(file+"\n")        
         f_counter = f_counter + 1 
         print("Extraction Complete\t:\t",file)
-        # cmd = "rm source.tar.gz temp_journals_list"
-        # os.system(cmd)        
+        cmd = "rm source.tar.gz temp_journals_list"
+        os.system(cmd)        
         time.sleep(.5)
     progress(1,1, status = "Complete")    
 
